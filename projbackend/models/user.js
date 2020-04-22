9ddbc722bf5c75530d83c47e406102d7935fdf6d
Prathamesh Mutkure <pmutkure009@gmail.com>
2020-04-22 17:52:04 +0530

    BACKEND: Create Models for DB

---
+++ projbackend/models/user.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+const uuidv1 = require('uuid/v1')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastName: {
+        type: 32,
+        maxlength: 32,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    userinfo: {
+        type: String,
+        trim: true
+    },
+    encry_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    purchases: {
+        type: Array,
+        default: []
+    }
+}, { timestamps: true })
+
+userSchema.virtual("password")
+    .set(function (plainPassword) {
+        this._password = plainPassword
+        this.salt = uuidv1()
+        this.encry_password = this.securePassword(plainPassword)
+    })
+    .get(function () {
+        return this._password
+    })
+
+userSchema.method = {
+
+    securePassword: function (plainPassword) {
+        if (!plainPassword) return ""
+        try {
+            return crypto
+                .createHmac('sha256', this.salt)
+                .update(plainPassword)
+                .digest('hex');
+        } catch (err) {
+            console.log(err)
+            return ""
+        }
+    },
+
+    authenticate: function (plainPassword) {
+        return this.encry_password === this.securePassword(plainPassword)
+    }
+}
+
+module.exports = mongoose.model("User", userSchema)
