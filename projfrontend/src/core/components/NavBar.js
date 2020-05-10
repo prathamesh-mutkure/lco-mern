3864cb02db588c110d0b31366fbd2732fd467ae0
Prathamesh Mutkure <pmutkure009@gmail.com>
2020-05-10 15:54:26 +0530

    FRONTEND: Updated NavBar

--- projfrontend/src/core/components/NavBar.js
+++ projfrontend/src/core/components/NavBar.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
-
-function isActive(history, path) {
-  if (history.location.path === path) return "active";
-  else return "";
-}
+import { Link, NavLink, withRouter } from "react-router-dom";
+import { isAuthenticated, signout } from "../../auth/helper";
 
 const Menu = ({ history }) => (
   <nav className="navbar navbar-expand-md navbar-dark bg-dark">
@@ -25,57 +21,76 @@ const Menu = ({ history }) => (
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link className={"nav-link " + isActive(history, "/")} to="/">
+          <NavLink
+            exact
+            className="nav-link"
+            activeClassName="active-nav-link"
+            to="/"
+          >
             Home
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <Link className={"nav-link " + isActive(history, "/cart")} to="/cart">
+          <NavLink
+            className="nav-link"
+            activeClassName="active-nav-link"
+            to="/cart"
+          >
             Cart
-          </Link>
+          </NavLink>
         </li>
-        <li className="nav-item">
-          <Link
-            className={"nav-link " + isActive(history, "/user/dashboard")}
-            to="/user/dashboard"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            className={"nav-link " + isActive(history, "/admin/dashboard")}
-            to="/admin/dashboard"
-          >
-            A. Dashboard
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            className={"nav-link " + isActive(history, "/signup")}
-            to="/signup"
-          >
-            Sign Up
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            className={"nav-link " + isActive(history, "/signin")}
-            to="/signin"
-          >
-            Sign In
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            className={"nav-link " + isActive(history, "/signout")}
-            to="/signout"
-          >
-            Sign Out
-          </Link>
-        </li>
+
+        {isAuthenticated() && isAuthenticated().user.role === 0 && (
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-nav-link"
+              to="/user/dashboard"
+            >
+              Dashboard
+            </NavLink>
+          </li>
+        )}
+
+        {isAuthenticated() && isAuthenticated().user.role === 1 && (
+          <li className="nav-item">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-nav-link"
+              to="/admin/dashboard"
+            >
+              A. Dashboard
+            </NavLink>
+          </li>
+        )}
       </ul>
     </div>
+
+    {isAuthenticated() ? (
+      <button
+        className="btn btn-success nav-btn"
+        onClick={() => {
+          signout(() => history.push("/"));
+        }}
+      >
+        Sign Out
+      </button>
+    ) : (
+      <div>
+        <button
+          className="btn btn-success nav-btn"
+          onClick={() => history.push("/signup")}
+        >
+          Sign Up
+        </button>
+        <button
+          className="btn btn-success nav-btn"
+          onClick={() => history.push("/signin")}
+        >
+          Sign In
+        </button>
+      </div>
+    )}
   </nav>
 );
 
